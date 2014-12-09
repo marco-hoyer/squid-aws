ae29aa2b0cb192117d60a9aa07950ff7dd25ee53
Marco Hoyer <marco@hoyer.pm>
2014-12-09 15:41:34 +0100

    remove pybuilder target dir

--- target/dist/squid-dynamodb-0.1/build/scripts-2.6/squid-dynamodb-auth.py
+++
@@ -1,7 +0,0 @@
-#!/data/home/mhoyer/virtualenv_2.6/bin/python
-
-from squid_dynamodb.auth_helper import DynamoDbAuthHelper
-
-if __name__ == "__main__":
-    authhelper = DynamoDbAuthHelper()
-    authhelper.main_loop()
--- target/dist/squid-dynamodb-0.1/scripts/squid-dynamodb-auth.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from squid_dynamodb.auth_helper import DynamoDbAuthHelper
-
-if __name__ == "__main__":
-    authhelper = DynamoDbAuthHelper()
-    authhelper.main_loop()
